51c18c7d68ec32a26a5ed3622e6a653fb410e0d3
romikette <romikette@gmail.com>
2020-02-23 20:28:55 +0200

    netlifying

---
+++ gatsby-config.js
@@ -0,0 +1,70 @@
+module.exports = {
+  siteMetadata: {
+    title: `Gatsby WordPress Client`,
+    description: `Gatsby client for headless WordPress API. This application is a POC`,
+    author: `romikette`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`,
+      },
+    },
+    {
+      resolve: "gatsby-source-wordpress",
+      options: {
+        // wordpress api
+        baseUrl: "gatsbypress.iamtimsmith.com",
+        protocol: "https",
+        hostingWPCOM: false,
+        useACF: true,
+        acfOptionPageIds: [],
+        verboseOutput: false,
+        perPage: 100,
+        searchAndReplaceContentUrls: {
+          sourceUrl: "https://gatsbypress.iamtimsmith.com",
+          replacementUrl: "https://localhost:8000",
+        },
+        // nr of how many concurrent requests can be sent
+        concurrentRequests: 10,
+        includedRoutes: [
+          "**/categories",
+          "**/posts",
+          "**/pages",
+          "**/media",
+          "**/tags",
+          "**/taxonomies",
+          "**/users",
+          "**/*/*/menus", // <== Menu api endpoint
+          "**/*/*/menu-locations", // <== Menu api endpoint
+        ],
+        excludedRoutes: [],
+        normalizer: function({ entities }) {
+          return entities
+        },
+      },
+    },
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-sitemap`,
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // 'gatsby-plugin-offline',
+  ],
+}
---
+++ gatsby-node.js
@@ -0,0 +1,50 @@
+const path = require(`path`)
+exports.createPages = async ({ graphql, actions, reporter }) => {
+  const { createPage } = actions
+  const BlogPostTemplate = path.resolve("./src/templates/BlogPost.js")
+  const PageTemplate = path.resolve("./src/templates/Page.js")
+  const result = await graphql(`
+    {
+      allWordpressPost {
+        edges {
+          node {
+            slug
+            wordpress_id
+          }
+        }
+      }
+      allWordpressPage {
+        edges {
+          node {
+            slug
+            wordpress_id
+          }
+        }
+      }
+    }
+  `)
+  if (result.errors) {
+    reporter.panicOnBuild(`Error while running GraphQL query.`)
+    return
+  }
+  const BlogPosts = result.data.allWordpressPost.edges
+  BlogPosts.forEach(post => {
+    createPage({
+      path: `/post/${post.node.slug}`,
+      component: BlogPostTemplate,
+      context: {
+        id: post.node.wordpress_id,
+      },
+    })
+    const Pages = result.data.allWordpressPage.edges
+    Pages.forEach(page => {
+      createPage({
+        path: `/${page.node.slug}`,
+        component: PageTemplate,
+        context: {
+          id: page.node.wordpress_id,
+        },
+      })
+    })
+  })
+}
